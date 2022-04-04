4a3f81de6dd7ccba1a4f0b80fd37090fada87982
Wirusik <wirus006@gmail.com>
2022-04-04 21:33:47 +0200

    define basic containers

---
+++ frontend/src/utils/containers.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const sets = styled.div`
+  padding: ${({setPadding}) => setPadding ? setPadding : '0'};
+  margin: ${({setMargin}) => setMargin ? setMargin : '0'};
+  width: ${({setWidth}) => setWidth ? setWidth : 'auto'};
+  max-width: ${({setMaxWidth}) => setMaxWidth ? setMaxWidth : 'auto'};
+  min-width: ${({setMinWidth}) => setMinWidth ? setMinWidth : 'auto'};
+  height: ${({setHeight}) => setHeight ? setHeight : 'auto'};
+  max-height: ${({setMaxHeight}) => setMaxHeight ? setMaxHeight : 'auto'};
+  min-height: ${({setMinHeight}) => setMinHeight ? setMinHeight : 'auto'};
+`;
+
+const FlexRow = styled(sets)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FlexColumn = styled(FlexRow)`
+  flex-direction: column;
+`;
+
+export {FlexRow, FlexColumn};
